5753114c36a933dbb88bf42c80694d684a5df48d
Crys <mariano.christopher123@gmail.com>
2023-04-06 19:04:54 +0800

    added tests

--- src/backend/test/nftMarketplace.test.js
+++ src/backend/test/nftMarketplace.test.js
@@ -3,6 +3,7 @@ const { expect } = require("chai");
 describe("nftMarketplace", async () => {
   let deployer, addr1, addr2;
   nft, marketplace;
+  let feePercent = 1;
   beforeEach(async () => {
     // Get Signers
     [deployer, addr1, addr2] = await ethers.getSigners();
@@ -16,6 +17,10 @@ describe("nftMarketplace", async () => {
     it("Should track name and symbol of the nft collection", async () => {
       expect(await nft.name()).to.equal("DApp NFT");
       expect(await nft.symbol()).to.equal("DAPP");
+    });
+    it("Should track feeAccount and feePercent of the marketplace", async () => {
+      expect(await marketplace.feeAccount()).to.equal(deployer.address);
+      expect(await marketplace.feePercent()).to.equal(feePercent);
     });
   });
 });
