801714b0e9f1d767c02ed5039365e62537970d3d
Crys <mariano.christopher123@gmail.com>
2023-04-08 07:42:06 +0800

    edit marketplace contract

--- src/backend/contracts/Marketplace.sol
+++ src/backend/contracts/Marketplace.sol
@@ -4,13 +4,60 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-contract Marketplace {
+error PriceMustNotBeZero();
+
+contract Marketplace is ReentrancyGuard {
     // State Variables
     address payable public immutable feeAccount;
     uint256 public immutable feePercent;
+    uint256 public itemCount;
+    struct Item {
+        uint256 itemId;
+        IERC721 nft;
+        uint256 tokenId;
+        uint256 price;
+        address payable seller;
+        bool sold;
+    }
+
+    event Offered(
+        uint256 itemId,
+        address indexed nft,
+        uint256 tokenId,
+        uint256 price,
+        address indexed seller
+    );
+
+    //ItemId(key) -> Item(return value)
+    mapping(uint256 => Item) public items;
 
     constructor(uint256 _feePercent) {
         feeAccount = payable(msg.sender);
         feePercent = _feePercent;
+    }
+
+    function makeItem(
+        IERC721 _nft,
+        uint256 _tokenId,
+        uint256 _price
+    ) external nonReentrant {
+        if (_price < 1) {
+            revert PriceMustNotBeZero();
+        }
+        // increment itemCount
+        itemCount++;
+        // transfer nft
+        _nft.transferFrom(msg.sender, address(this), _tokenId);
+        //add new item to items mapping
+        items[itemCount] = Item(
+            itemCount,
+            _nft,
+            _tokenId,
+            _price,
+            payable(msg.sender),
+            false
+        );
+        // emit Offered event
+        emit Offered(itemCount, address(_nft), _tokenId, _price, msg.sender);
     }
 }
