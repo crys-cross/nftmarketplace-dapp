cb1c05ba19cf1093459a447d5dca8190524df5a9
Crys <mariano.christopher123@gmail.com>
2023-04-14 11:56:24 +0800

    edited frontend

--- src/frontend/components/App.js
+++ src/frontend/components/App.js
@@ -1,8 +1,45 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navigation from "./Navbar";
+import Home from "./Home.js";
+import Create from "./Create.js";
+import MyListedItems from "./MyListedItems.js";
+import MyPurchases from "./MyPurchases.js";
+import MarketplaceAbi from "../contractsData/Marketplace.json";
+import MarketplaceAddress from "../contractsData/Marketplace-address.json";
+import NFTAbi from "../contractsData/NFT.json";
+import NFTAddress from "../contractsData/NFT-address.json";
+import { useState } from "react";
+import { ethers } from "ethers";
+import { Spinner } from "react-bootstrap";
 
-import logo from './logo.png';
-import './App.css';
- 
 function App() {
+  const [account, setAccount] = useState(null);
+  // metamask Login/Connect
+  const web3Handler = async () => {
+    const accounts = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+    setAccount(accounts[0]);
+    // get provider from metamask
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    // set signer
+    const signer = provider.getSigner();
+
+    loadContracts(signer);
+  };
+
+  const loadContracts = async (signer) => {
+    // get deployed copies of contracts
+    const marketplace = new ethers.Contract(
+      MarketplaceAddress.address,
+      MarketplaceAbi.abi,
+      signer
+    );
+    setMarketplace(marketplace);
+    const nft = new ethers.Contract(NFTAddress.address, NFTAbi.abi, signer);
+    setNFT(nft);
+  };
+
   return (
     <div>
       <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
@@ -24,11 +61,12 @@ function App() {
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                <img src={logo} className="App-logo" alt="logo"/>
+                <img src={logo} className="App-logo" alt="logo" />
               </a>
-              <h1 className= "mt-5">Dapp University Starter Kit</h1>
+              <h1 className="mt-5">Dapp University Starter Kit</h1>
               <p>
-                Edit <code>src/frontend/components/App.js</code> and save to reload.
+                Edit <code>src/frontend/components/App.js</code> and save to
+                reload.
               </p>
               <a
                 className="App-link"
@@ -36,7 +74,10 @@ function App() {
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                LEARN BLOCKCHAIN <u><b>NOW! </b></u>
+                LEARN BLOCKCHAIN{" "}
+                <u>
+                  <b>NOW! </b>
+                </u>
               </a>
             </div>
           </main>
