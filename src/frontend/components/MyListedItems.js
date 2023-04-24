589aa09f657bd7a46f0c10993d3f641af7d0b75b
Crys <mariano.christopher123@gmail.com>
2023-04-24 12:49:59 +0800

    updated mylisteditems frontend component

--- src/frontend/components/MyListedItems.js
+++ src/frontend/components/MyListedItems.js
@@ -2,6 +2,27 @@ import { useState, useEffect } from "react";
 import { ethers } from "hardhat";
 import { Row, Col, Card } from "react-bootstrap";
 
+const renderSoldItems = (items) => {
+  return (
+    <>
+      <h2>Sold</h2>
+      <Row xs={1} md={2} lg={4} className="g-4 py-3">
+        {items.map((item, idx) => (
+          <Col key={idx} className="overflow-hidden">
+            <Card>
+              <Card.Img variant="top" src={item.image} />
+              <Card.Footer>
+                For {ethers.utils.formatEther(item.totalPrice)} ETH - Recieved{" "}
+                {ethers.utils.formatEther(item.price)} ETH
+              </Card.Footer>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
+};
+
 const MyListedItems = ({ marketplace, nft, account }) => {
   const [loading, setLoading] = useState(true);
   const [listedItems, setLitedItems] = useState([]);
@@ -52,7 +73,32 @@ const MyListedItems = ({ marketplace, nft, account }) => {
       </main>
     );
 
-  return <div>MyListedItems</div>;
+  return (
+    <div className="flex justify-center">
+      {listedItems.length > 0 ? (
+        <div className="px-5 py-3 container">
+          <h2>Listed</h2>
+          <Row xs={1} md={2} lg={4} className="g-4 py-3">
+            {listedItems.map((item, idx) => (
+              <Col key={idx} className="overflow-hidden">
+                <Card>
+                  <Card.Img variant="top" src={item.image} />
+                  <Card.Footer>
+                    {ethers.utils.formatEther(item.totalPrice)} ETH
+                  </Card.Footer>
+                </Card>
+              </Col>
+            ))}
+          </Row>
+          {soldItems.length > 0 && renderSoldItems(soldItems)}
+        </div>
+      ) : (
+        <main style={{ padding: "1rem 0" }}>
+          <h2>No listed assets</h2>
+        </main>
+      )}
+    </div>
+  );
 };
 
 export default MyListedItems;
